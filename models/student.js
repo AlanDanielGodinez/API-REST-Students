d3ae667e23dce2c9dd015ec9e6e314970a8b0e34
Yael SG <yaeldh8@gmail.com>
2024-02-29 08:41:09 -0600

    Modelo estudiante

---
+++ models/student.js
@@ -0,0 +1,9 @@
+module.exports = class Student {
+    constructor(nc, ap_Paterno,ap_materno, nombres, promedio){
+        this.nc = nc;
+        this.ap_Paterno = ap_Paterno;
+        this.ap_materno = ap_materno;
+        this.nombres = nombres;
+        this.promedio = promedio;
+    }
+}
