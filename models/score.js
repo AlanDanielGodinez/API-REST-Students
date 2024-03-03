e2d5bf4f2048998839a35a28563e2458309dc8ba
Yael SG <yaeldh8@gmail.com>
2024-03-03 01:00:40 -0600

    P: promedios

---
+++ models/score.js
@@ -0,0 +1,12 @@
+module.exports = class Score{
+    constructor(score1, score2, score3, score4){
+        this.score1 = score1;
+        this.score2 = score2;
+        this.score3 = score3;
+        this.score4 = score4;
+    }
+
+    avgScore() {
+        
+    }
+}
